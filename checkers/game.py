d277d327bebed29dfd2cbf3f867db0d185f727c8
oscar <oscarleonardopedrozo@gmail.com>
2022-09-26 20:16:13 -0400

    movimientos de fichas

--- checkers/game.py
+++ checkers/game.py
@@ -1,5 +1,5 @@
 import pygame
-from .constants import BLACK, WHITE
+from .constants import BLACK, WHITE, SQUARE_SIZE, GREEN
 from .board import Board
 
 class Game:
@@ -9,6 +9,7 @@ class Game:
 
     def update(self):
         self.board.draw(self.win)
+        self.draw_valid_moves(self.valid_moves)
         pygame.display.update()
 
     def _init(self):
@@ -16,6 +17,9 @@ class Game:
         self.board = Board()
         self.turn = BLACK
         self.valid_moves = {}
+
+    def winner(self):
+        return self.board.winner()
 
     def reset(self):
        self._init()
@@ -29,31 +33,40 @@ class Game:
             if not result:
                 self.selected = None
                 self.select(row, col)
-        # Si no hay pieza seleccionada
-        else:
-            piece = self.board.get_piece(row, col)
-            if piece != 0 and piece.color == self.turn:
-                self.selected = piece
-                self.valid_moves = self.board.get_valid_moves(piece)
-                return True
+
+        piece = self.board.get_piece(row, col)
+        if piece != 0 and piece.color == self.turn:
+            self.selected = piece
+            self.valid_moves = self.board.get_valid_moves(piece)
+            return True
 
         return False
 
-    def move(self, row, col):
+    def _move(self, row, col):
         piece = self.board.get_piece(row, col)
         # Si es 0 no hay pieza seleccionada
         if self.selected and piece == 0 and (row, col) in self.valid_moves:
             #mover la pieza seleccionada a la fila y columna
             self.board.move(self.selected, row, col)
+            skipped = self.valid_moves[(row, col)]
+            if skipped:
+                self.board.remove(skipped)
             self.change_turn()
         else:
             return False
 
         return True
     
+    def draw_valid_moves(self, moves):
+        for move in moves:
+            row, col = move
+            pygame.draw.circle(self.win, GREEN , (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), 15)
+
     def change_turn(self):
         self.valid_moves = {}
         if self.turn == BLACK:
             self.turn = WHITE
         else:
             self.turn = BLACK
+
+    
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE
+from checkers.constants import BLACK, WIDTH, HEIGHT, SQUARE_SIZE
 from checkers.board import Board
 from checkers.game import Game
 
@@ -25,6 +25,11 @@ def main():
     #board.move(piece,7,3)
     while run:
         clock.tick(FPS)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+            
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -32,6 +37,7 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
 
         game.update()
     pygame.quit()
